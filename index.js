1f8a2b6786df20afec9bf4f86537b2727f07a880
MaddHacker <hacker@maddhacker.com>
2017-04-03 17:10:03 -0600

    update to handle v3.0.0 - also can now support multiple output streams... wheee!!

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const O = require('output-manager'); // console logging
+const datez = require('date-utilz');
+const om = require('output-manager'); // console logging
+const O = new om.Out();
+const fOut = new om.Out();
 const fs = require('fs');
 const os = require('os');
 
@@ -8,25 +11,24 @@ O.i('Starting...');
 
 const start = () => {
     O.i('Setting up log to file');
-    O.level(O.LogLevel.TRACE);
-    O.setLogger(function (msg) {
-        // do log to file
-        ws.write(msg + os.EOL);
-    });
+    fOut.level = om.LogLevel.TRACE;
+    fOut.output = (msg) => { /* do log to file */ ws.write(msg + os.EOL); };
     setTimeout(logSomeData, 1500);
 }
 
 const logSomeData = () => {
-    O.t('tracing');
-    O.d('debugging');
-    O.i('info-ing');
-    O.w('warning');
-    O.e('erroring');
-    O.f('fatal-ing');
+    O.i('Logging to file...');
+    fOut.t('tracing');
+    fOut.d('debugging');
+    fOut.i('info-ing');
+    fOut.w('warning');
+    fOut.e('erroring');
+    fOut.f('fatal-ing');
+    O.i('Sleeping...');
     setTimeout(logSomeData, 500);
 }
 
-const ws = fs.createWriteStream('./' + (new Date).toISOString().split('T')[0] + '.log');
+const ws = fs.createWriteStream('./' + datez.date().split('T')[0] + '.log');
 
 O.i('Everything started...');
 
